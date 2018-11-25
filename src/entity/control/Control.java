970fe60d56ed0632e3b735978bc94705a0b3744e
namkang321 <namkang_chutisilp@hotmail.com>
2018-11-25 22:52:17 +0700

    ADD getters setters for class Control

--- src/entity/control/Control.java
+++ src/entity/control/Control.java
@@ -17,4 +17,77 @@ public class Control {
 	public static KeyCode attackPlayer2 = KeyCode.DIGIT5;
 	public static KeyCode jumpPlayer2 = KeyCode.DIGIT9;
 	
+	public static KeyCode getMoveUpPlayer1() {
+		return moveUpPlayer1;
+	}
+	public static void setMoveUpPlayer1(KeyCode moveUpPlayer1) {
+		Control.moveUpPlayer1 = moveUpPlayer1;
+	}
+	public static KeyCode getMoveDownPlayer1() {
+		return moveDownPlayer1;
+	}
+	public static void setMoveDownPlayer1(KeyCode moveDownPlayer1) {
+		Control.moveDownPlayer1 = moveDownPlayer1;
+	}
+	public static KeyCode getMoveLeftPlayer1() {
+		return moveLeftPlayer1;
+	}
+	public static void setMoveLeftPlayer1(KeyCode moveLeftPlayer1) {
+		Control.moveLeftPlayer1 = moveLeftPlayer1;
+	}
+	public static KeyCode getMoveRightPlayer1() {
+		return moveRightPlayer1;
+	}
+	public static void setMoveRightPlayer1(KeyCode moveRightPlayer1) {
+		Control.moveRightPlayer1 = moveRightPlayer1;
+	}
+	public static KeyCode getAttackPlayer1() {
+		return attackPlayer1;
+	}
+	public static void setAttackPlayer1(KeyCode attackPlayer1) {
+		Control.attackPlayer1 = attackPlayer1;
+	}
+	public static KeyCode getJumpPlayer1() {
+		return jumpPlayer1;
+	}
+	public static void setJumpPlayer1(KeyCode jumpPlayer1) {
+		Control.jumpPlayer1 = jumpPlayer1;
+	}
+	public static KeyCode getMoveUpPlayer2() {
+		return moveUpPlayer2;
+	}
+	public static void setMoveUpPlayer2(KeyCode moveUpPlayer2) {
+		Control.moveUpPlayer2 = moveUpPlayer2;
+	}
+	public static KeyCode getMoveDownPlayer2() {
+		return moveDownPlayer2;
+	}
+	public static void setMoveDownPlayer2(KeyCode moveDownPlayer2) {
+		Control.moveDownPlayer2 = moveDownPlayer2;
+	}
+	public static KeyCode getMoveLeftPlayer2() {
+		return moveLeftPlayer2;
+	}
+	public static void setMoveLeftPlayer2(KeyCode moveLeftPlayer2) {
+		Control.moveLeftPlayer2 = moveLeftPlayer2;
+	}
+	public static KeyCode getMoveRightPlayer2() {
+		return moveRightPlayer2;
+	}
+	public static void setMoveRightPlayer2(KeyCode moveRightPlayer2) {
+		Control.moveRightPlayer2 = moveRightPlayer2;
+	}
+	public static KeyCode getAttackPlayer2() {
+		return attackPlayer2;
+	}
+	public static void setAttackPlayer2(KeyCode attackPlayer2) {
+		Control.attackPlayer2 = attackPlayer2;
+	}
+	public static KeyCode getJumpPlayer2() {
+		return jumpPlayer2;
+	}
+	public static void setJumpPlayer2(KeyCode jumpPlayer2) {
+		Control.jumpPlayer2 = jumpPlayer2;
+	}
+	
 }
