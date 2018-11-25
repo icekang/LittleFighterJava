35b1f147a54b25492264c6d147f6fb4ac2094f22
namkang321 <namkang_chutisilp@hotmail.com>
2018-11-25 22:50:01 +0700

    entity.control containing Control Class for players

---
+++ src/entity/control/Control.java
@@ -0,0 +1,20 @@
+package entity.control;
+
+import javafx.scene.input.KeyCode;
+
+public class Control {
+	public static KeyCode moveUpPlayer1 = KeyCode.W;
+	public static KeyCode moveDownPlayer1 = KeyCode.S;
+	public static KeyCode moveLeftPlayer1 = KeyCode.A;
+	public static KeyCode moveRightPlayer1 = KeyCode.D;
+	public static KeyCode attackPlayer1 = KeyCode.J;
+	public static KeyCode jumpPlayer1 = KeyCode.K;
+	
+	public static KeyCode moveUpPlayer2 = KeyCode.UP;
+	public static KeyCode moveDownPlayer2 = KeyCode.DOWN;
+	public static KeyCode moveLeftPlayer2 = KeyCode.LEFT;
+	public static KeyCode moveRightPlayer2 = KeyCode.RIGHT;
+	public static KeyCode attackPlayer2 = KeyCode.DIGIT5;
+	public static KeyCode jumpPlayer2 = KeyCode.DIGIT9;
+	
+}
